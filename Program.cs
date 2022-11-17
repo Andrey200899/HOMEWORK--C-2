396342b16283e35ac8a99cfa8b0271f28fa6c3d2
unknown <kvkvachev@yandex.ru>
2022-11-17 08:16:37 +0300

    задача 10

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("Введите число :");
+string Numbers =Console.ReadLine()!;
+if (Numbers.Length !=3){
+    Console.WriteLine("Введено неверное число");
+}
+else
+ {
+    Console.WriteLine($"Вторая цифры равна: {Numbers[1]}");
+ }
+
+
